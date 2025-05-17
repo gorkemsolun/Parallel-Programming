1da348b311058f18519b703fddecc0bf0447ff9d
gorkemsolun <gorkemkadir14@hotmail.com>
2025-05-17 21:11:53 +0300

    Add .gitignore and update output files for Conjugate Gradient and Checkerboard Partitioning: included .vscode directory in .gitignore, removed outdated configuration files, and updated output results for both algorithms to reflect new performance metrics.

--- Conjugate-Gradient/parallel.c
+++ Conjugate-Gradient/parallel.c
@@ -9,55 +9,62 @@
 #include <math.h>
 
 #define MASTER 0
-#define DISTRIBUTE_TAG  100
+#define DISTRIBUTE_TAG 100
 #define P_SEND_TAG 200
 #define RESULT_SEND_TAG 300
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     MPI_Init(&argc, &argv);
     int world_rank, world_size;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    if (argc < 2 || argc > 3) {
-        if (world_rank == MASTER) {
+    if (argc < 2 || argc > 3)
+    {
+        if (world_rank == MASTER)
+        {
             fprintf(stderr, "Usage: %s <input_file> [output_file]\n", argv[0]);
-
         }
         MPI_Finalize();
         return EXIT_FAILURE;
-
     }
-    const char* input_file_name = argv[1];
-    const char* output_file_name = (argc == 3) ? argv[2] : "p_output.txt";
+    const char *input_file_name = argv[1];
+    const char *output_file_name = (argc == 3) ? argv[2] : "p_output.txt";
 
-    if (world_size < 2) {
-        if (world_rank == MASTER) {
+    if (world_size < 2)
+    {
+        if (world_rank == MASTER)
+        {
             fprintf(stderr, "Need >=2 MPI ranks\n");
         }
-        MPI_Finalize(); return 1;
+        MPI_Finalize();
+        return 1;
     }
 
     // Split out a communicator for all the workers (ranks 1..size-1)
     MPI_Comm worker_comm;
     MPI_Comm_split(MPI_COMM_WORLD,
-        world_rank == MASTER ? MPI_UNDEFINED : 1,
-        world_rank,
-        &worker_comm);
+                   world_rank == MASTER ? MPI_UNDEFINED : 1,
+                   world_rank,
+                   &worker_comm);
 
     int n, rows_per_worker;
-    double* A = NULL, * b = NULL;
+    double *A = NULL, *b = NULL;
 
-    if (world_rank == MASTER) {
-        FILE* input_file = fopen(input_file_name, "r");
-        if (!input_file) {
+    if (world_rank == MASTER)
+    {
+        FILE *input_file = fopen(input_file_name, "r");
+        if (!input_file)
+        {
             perror("fopen");
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
         fscanf(input_file, "%d", &n);
 
         // Check if n is divisible by world_size - 1
-        if (n % (world_size - 1) != 0) {
+        if (n % (world_size - 1) != 0)
+        {
             fprintf(stderr, "Error: n (%d) is not divisible by world_size - 1 (%d)\n", n, world_size - 1);
             fclose(input_file);
             MPI_Abort(MPI_COMM_WORLD, 1);
@@ -67,17 +74,21 @@ int main(int argc, char* argv[]) {
         rows_per_worker = n / (world_size - 1);
         A = malloc(n * n * sizeof(double));
         b = malloc(n * sizeof(double));
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
                 fscanf(input_file, "%lf", &A[i * n + j]);
             }
         }
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             fscanf(input_file, "%lf", &b[i]);
         }
         fclose(input_file);
 
-        for (int w = 1; w < world_size; ++w) {
+        for (int w = 1; w < world_size; ++w)
+        {
             MPI_Send(&n, 1, MPI_INT, w, DISTRIBUTE_TAG, MPI_COMM_WORLD);
             MPI_Send(&rows_per_worker, 1, MPI_INT, w, DISTRIBUTE_TAG, MPI_COMM_WORLD);
 
@@ -92,15 +103,17 @@ int main(int argc, char* argv[]) {
         MPI_Barrier(MPI_COMM_WORLD);
         double t_end = MPI_Wtime();
 
-        double* result = malloc(n * sizeof(double));
-        for (int w = 1; w < world_size; ++w) {
+        double *result = malloc(n * sizeof(double));
+        for (int w = 1; w < world_size; ++w)
+        {
             int offset = (w - 1) * rows_per_worker;
             MPI_Recv(result + offset, rows_per_worker, MPI_DOUBLE, w, RESULT_SEND_TAG,
-                MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
 
-        FILE* output_file = fopen(output_file_name, "w");
-        for (int i = 0; i < n; ++i) {
+        FILE *output_file = fopen(output_file_name, "w");
+        for (int i = 0; i < n; ++i)
+        {
             fprintf(output_file, "%.8f\n", result[i]);
         }
 
@@ -111,16 +124,18 @@ int main(int argc, char* argv[]) {
         free(A);
         free(b);
         free(result);
-    } else {
+    }
+    else
+    {
         MPI_Recv(&n, 1, MPI_INT, MASTER, DISTRIBUTE_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(&rows_per_worker, 1, MPI_INT, MASTER, DISTRIBUTE_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        double* A_loc = malloc(rows_per_worker * n * sizeof(double));
-        double* b_loc = malloc(rows_per_worker * sizeof(double));
+        double *A_loc = malloc(rows_per_worker * n * sizeof(double));
+        double *b_loc = malloc(rows_per_worker * sizeof(double));
         MPI_Recv(A_loc, rows_per_worker * n, MPI_DOUBLE, MASTER, DISTRIBUTE_TAG,
-            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         MPI_Recv(b_loc, rows_per_worker, MPI_DOUBLE, MASTER, DISTRIBUTE_TAG,
-            MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                 MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
         MPI_Barrier(MPI_COMM_WORLD);
 
@@ -128,13 +143,14 @@ int main(int argc, char* argv[]) {
         MPI_Comm_rank(worker_comm, &worker_rank);
         MPI_Comm_size(worker_comm, &worker_size);
 
-        double* x_loc = calloc(rows_per_worker, sizeof(double));
-        double* r_loc = malloc(rows_per_worker * sizeof(double));
-        double* p_loc = malloc(rows_per_worker * sizeof(double));
-        double* q_loc = malloc(rows_per_worker * sizeof(double));
+        double *x_loc = calloc(rows_per_worker, sizeof(double));
+        double *r_loc = malloc(rows_per_worker * sizeof(double));
+        double *p_loc = malloc(rows_per_worker * sizeof(double));
+        double *q_loc = malloc(rows_per_worker * sizeof(double));
 
         // initial r=b, p=r
-        for (int i = 0; i < rows_per_worker; ++i) {
+        for (int i = 0; i < rows_per_worker; ++i)
+        {
             r_loc[i] = b_loc[i];
             p_loc[i] = r_loc[i];
         }
@@ -149,49 +165,58 @@ int main(int argc, char* argv[]) {
         r_dot_r_rho = r_dot_new; */
 
         // full p and r location
-        double* p_full = malloc(n * sizeof(double));
+        double *p_full = malloc(n * sizeof(double));
 
         double r_dot, p_dot_q, r_dot_new, alpha, beta;
-        for (int iteration = 0; iteration < n; ++iteration) {
+        int iteration = 0;
+        while (1)
+        {
             int my_offset = worker_rank * rows_per_worker;
-            for (int i = 0; i < rows_per_worker; ++i) {
+            for (int i = 0; i < rows_per_worker; ++i)
+            {
                 p_full[my_offset + i] = p_loc[i];
             }
 
             // post receives from all peers, then sends
-            MPI_Request* requests = malloc(2 * (worker_size - 1) * sizeof(MPI_Request));
+            MPI_Request *requests = malloc(2 * (worker_size - 1) * sizeof(MPI_Request));
             int request_counter = 0;
-            for (int peer = 0;peer < worker_size; ++peer) {
-                if (peer == worker_rank) {
+            for (int peer = 0; peer < worker_size; ++peer)
+            {
+                if (peer == worker_rank)
+                {
                     continue;
                 }
 
                 int peer_offset = peer * rows_per_worker;
                 MPI_Irecv(p_full + peer_offset, rows_per_worker, MPI_DOUBLE,
-                    peer + 1, P_SEND_TAG, MPI_COMM_WORLD,
-                    &requests[request_counter++]);
+                          peer + 1, P_SEND_TAG, MPI_COMM_WORLD,
+                          &requests[request_counter++]);
             }
 
-            for (int peer = 0; peer < worker_size; ++peer) {
-                if (peer == worker_rank) {
+            for (int peer = 0; peer < worker_size; ++peer)
+            {
+                if (peer == worker_rank)
+                {
                     continue;
                 }
 
                 MPI_Isend(p_loc, rows_per_worker, MPI_DOUBLE,
-                    peer + 1, P_SEND_TAG, MPI_COMM_WORLD,
-                    &requests[request_counter++]);
+                          peer + 1, P_SEND_TAG, MPI_COMM_WORLD,
+                          &requests[request_counter++]);
             }
 
-            if (request_counter > 0) {
+            if (request_counter > 0)
+            {
                 MPI_Waitall(request_counter, requests, MPI_STATUSES_IGNORE);
             }
             free(requests);
 
-
             // local SpMV q_loc = A_loc * p_full
-            for (int i = 0; i < rows_per_worker; ++i) {
+            for (int i = 0; i < rows_per_worker; ++i)
+            {
                 double sum = 0;
-                for (int j = 0; j < n; ++j) {
+                for (int j = 0; j < n; ++j)
+                {
                     sum += A_loc[i * n + j] * p_full[j];
                 }
                 q_loc[i] = sum;
@@ -200,7 +225,8 @@ int main(int argc, char* argv[]) {
             // local dot products
             p_dot_q = 0;
             r_dot = 0;
-            for (int i = 0;i < rows_per_worker; ++i) {
+            for (int i = 0; i < rows_per_worker; ++i)
+            {
                 p_dot_q += p_loc[i] * q_loc[i];
                 r_dot += r_loc[i] * r_loc[i];
             }
@@ -209,41 +235,48 @@ int main(int argc, char* argv[]) {
             MPI_Allreduce(&p_dot_q, &sum_pq, 1, MPI_DOUBLE, MPI_SUM, worker_comm);
             MPI_Allreduce(&r_dot, &sum_rr, 1, MPI_DOUBLE, MPI_SUM, worker_comm);
 
-
-            if (iteration == 0) {
+            if (iteration == 0)
+            {
                 alpha = sum_rr / sum_pq;
-            } else {
+            }
+            else
+            {
                 beta = sum_rr / r_dot;
                 alpha = sum_rr / sum_pq;
             }
 
             // update
-            for (int i = 0; i < rows_per_worker; ++i) {
+            for (int i = 0; i < rows_per_worker; ++i)
+            {
                 x_loc[i] += alpha * p_loc[i];
                 r_loc[i] -= alpha * q_loc[i];
             }
 
             r_dot_new = 0;
-            for (int i = 0; i < rows_per_worker; ++i) {
+            for (int i = 0; i < rows_per_worker; ++i)
+            {
                 r_dot_new += r_loc[i] * r_loc[i];
             }
             MPI_Allreduce(MPI_IN_PLACE, &r_dot_new, 1, MPI_DOUBLE, MPI_SUM, worker_comm);
 
-            if (sqrt(r_dot_new) < 1e-8) {
+            if (sqrt(r_dot_new) < 1e-13)
+            {
                 break;
             }
 
             beta = r_dot_new / sum_rr;
-            for (int i = 0;i < rows_per_worker; ++i) {
+            for (int i = 0; i < rows_per_worker; ++i)
+            {
                 p_loc[i] = r_loc[i] + beta * p_loc[i];
             }
             r_dot = r_dot_new;
+            ++iteration;
         }
 
         MPI_Barrier(MPI_COMM_WORLD);
 
         MPI_Send(x_loc, rows_per_worker, MPI_DOUBLE,
-            MASTER, RESULT_SEND_TAG, MPI_COMM_WORLD);
+                 MASTER, RESULT_SEND_TAG, MPI_COMM_WORLD);
 
         free(A_loc);
         free(b_loc);
--- Conjugate-Gradient/serial.c
+++ Conjugate-Gradient/serial.c
@@ -1,4 +1,4 @@
-// Görkem Kadir Solun 
+// Görkem Kadir Solun
 // Serial Conjugate Gradient solver for Ax = b
 // Reads matrix A and vector b from input files, solves for x, and writes x to s_output.txt
 
@@ -7,51 +7,59 @@
 #include <math.h>
 #include <time.h>
 
-
-int main(int argc, char* argv[]) {
-    if (argc < 2 || argc > 3) {
+int main(int argc, char *argv[])
+{
+    if (argc < 2 || argc > 3)
+    {
         fprintf(stderr, "Usage: %s <input_file> [output_file]\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    const char* input_file_name = argv[1];
-    const char* outputFileName = (argc == 3) ? argv[2] : "s_output.txt";
-    FILE* file = fopen(input_file_name, "r");
-    if (!file) {
+    const char *input_file_name = argv[1];
+    const char *outputFileName = (argc == 3) ? argv[2] : "s_output.txt";
+    FILE *file = fopen(input_file_name, "r");
+    if (!file)
+    {
         perror("Error opening input file");
         return EXIT_FAILURE;
     }
 
     int N;
     // First value: dimension
-    if (fscanf(file, "%d", &N) != 1) {
+    if (fscanf(file, "%d", &N) != 1)
+    {
         fprintf(stderr, "Failed to read dimension\n");
         return EXIT_FAILURE;
     }
 
     // Allocate arrays
-    double* A = malloc(N * N * sizeof(double));
-    double* b = malloc(N * sizeof(double));
-    double* x = calloc(N, sizeof(double));  // x=0
-    double* r = malloc(N * sizeof(double));
-    double* p = malloc(N * sizeof(double));
-    double* Ap = malloc(N * sizeof(double));
-    if (!A || !b || !x || !r || !p || !Ap) {
+    double *A = malloc(N * N * sizeof(double));
+    double *b = malloc(N * sizeof(double));
+    double *x = calloc(N, sizeof(double)); // x=0
+    double *r = malloc(N * sizeof(double));
+    double *p = malloc(N * sizeof(double));
+    double *Ap = malloc(N * sizeof(double));
+    if (!A || !b || !x || !r || !p || !Ap)
+    {
         fprintf(stderr, "Memory allocation failed\n");
         return EXIT_FAILURE;
     }
 
     // Read matrix entries (row-major)
-    for (int i = 0; i < N * N; ++i) {
-        if (fscanf(file, "%lf", &A[i]) != 1) {
+    for (int i = 0; i < N * N; ++i)
+    {
+        if (fscanf(file, "%lf", &A[i]) != 1)
+        {
             fprintf(stderr, "Failed to read A[%d]\n", i);
             return EXIT_FAILURE;
         }
     }
 
     // Read vector b
-    for (int i = 0; i < N; ++i) {
-        if (fscanf(file, "%lf", &b[i]) != 1) {
+    for (int i = 0; i < N; ++i)
+    {
+        if (fscanf(file, "%lf", &b[i]) != 1)
+        {
             fprintf(stderr, "Failed to read b[%d]\n", i);
             return EXIT_FAILURE;
         }
@@ -61,70 +69,83 @@ int main(int argc, char* argv[]) {
     double alpha, beta, rho;
 
     // r = b - A*x (x=0 to r=b)
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
+    {
         r[i] = b[i];
         p[i] = r[i];
     }
     rho = 0;
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
+    {
         rho += r[i] * r[i];
     }
 
     clock_t t_start = clock();
 
     // Max iterations is N to avoid infinite loop
-    for (int k = 0; k < N; ++k) {
+    while (1)
+    {
         // Ap = A * p
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i)
+        {
             Ap[i] = 0.0;
-            for (int j = 0; j < N; ++j) {
+            for (int j = 0; j < N; ++j)
+            {
                 Ap[i] += A[i * N + j] * p[j];
             }
         }
 
         // alpha = rho / (p^T * Ap)   //   (i.e., pi = <p, Ap> and alpha = rho / pi)
         double pAp = 0;
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i)
+        {
             pAp += p[i] * Ap[i];
         }
         alpha = rho / pAp;
 
         // x = x + alpha * p
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i)
+        {
             x[i] += alpha * p[i];
         }
         // r = r - alpha * Ap
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i)
+        {
             r[i] -= alpha * Ap[i];
         }
 
         // convergence
         double rhoNew = 0;
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i)
+        {
             rhoNew += r[i] * r[i];
         }
-        if (sqrt(rhoNew) < 1e-6) {
+        if (sqrt(rhoNew) < 1e-13)
+        {
             break;
         }
 
         // p = r + (rhoNew/rho) * p
         beta = rhoNew / rho;
-        for (int i = 0; i < N; ++i) {
+        for (int i = 0; i < N; ++i)
+        {
             p[i] = r[i] + beta * p[i];
         }
         rho = rhoNew;
     }
 
     clock_t t_end = clock();
-    double elapsed = (double) (t_end - t_start) / CLOCKS_PER_SEC;
+    double elapsed = (double)(t_end - t_start) / CLOCKS_PER_SEC;
     fprintf(stderr, "Elapsed time: %f seconds\n", elapsed);
 
-    FILE* outputFile = fopen(outputFileName, "w");
-    if (!outputFile) {
+    FILE *outputFile = fopen(outputFileName, "w");
+    if (!outputFile)
+    {
         perror("Error opening output file");
         return EXIT_FAILURE;
     }
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
+    {
         fprintf(outputFile, "%lf\n", x[i]);
     }
 
